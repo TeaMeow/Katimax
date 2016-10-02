c5e4f83b0f460cb83405c45cd49cc2e7225abc4c
Yami Odymel <yamiodymel@gmail.com>
2016-10-03 04:15:01 +0800

    Update webpack.base.js

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path              = require('path')
+const webpack           = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
